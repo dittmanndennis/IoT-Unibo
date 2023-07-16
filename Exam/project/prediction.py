d1dbd57dfa5c8b10b6d03956087c2f57c2334229
dittmanndennis <dennis1dittmann@gmail.com>
2023-07-16 14:48:16 +0200

    finished prediction

--- Exam/project/prediction.py
+++ Exam/project/prediction.py
@@ -1,8 +1,10 @@
 import datetime
 import pandas as pd
 import numpy as np
+
 from statsmodels.tsa.api import VAR
 from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from statsmodels.tools.eval_measures import mse
 
 
 
@@ -14,7 +16,7 @@ def predict_alarm_in(alarm_level, time, weight, temperature, humidity):
     df_all = pd.DataFrame({'weight': weight, 'temperature': temperature, 'humidity': humidity}, time)
     df_all = df_all.asfreq(pd.infer_freq(df_all.index))
 
-    return __predict_on_weight(alarm_level, df_weight), __predict_on_all(alarm_level, df_all)
+    return (__predict_on_weight(alarm_level, df_weight), __mse_predict_on_weight(df_weight)), (__predict_on_all(alarm_level, df_all), __mse_predict_on_all(df_all))
 
 
 def __predict_on_weight(alarm_level, df_weight):
@@ -42,14 +44,14 @@ def __predict_on_weight(alarm_level, df_weight):
 
 
 def __predict_on_all(alarm_level, df_all):
-    if len(df_all) < 20: return None
-
     if df_all['weight'].iloc[-1] <= alarm_level:
         t = datetime.datetime.strptime("00:00:00","%H:%M:%S")
         return datetime.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
     
+    if len(df_all) < 20: return None
+    
     model = VAR(df_all)
-    model.select_order(int(len(df_all)/20))
+    #model.select_order(int(len(df_all)/20))
     results = model.fit(maxlags=int(len(df_all)/7))
     freq = pd.to_timedelta(np.diff(df_all.index).min()).total_seconds()
     forecast = results.forecast(df_all.values, int(86400 / freq))
@@ -65,11 +67,29 @@ def __predict_on_all(alarm_level, df_all):
     return datetime.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second)
 
 
-def __mse_predict_on_weight():
-    return None
+def __mse_predict_on_weight(df_weight):
+    df_train = df_weight.iloc[:int(0.8*len(df_weight))]
+    df_test = df_weight.iloc[int(0.8*len(df_weight)):]
 
-def __mse_predict_on_all():
-    return None
+    auto_lags = ar_select_order(df_train, int(len(df_train)/2-1)).ar_lags
+    model = AutoReg(df_train, auto_lags).fit()
+    predictions = model.predict(start=len(df_train), end=len(df_weight)-1)
+    
+    return mse(predictions.array, df_test['weight'].array)
+
+def __mse_predict_on_all(df_all):
+    if len(df_all) < 20: return None
+
+    df_train = df_all.iloc[:int(0.8*len(df_all))]
+    df_test = df_all.iloc[int(0.8*len(df_all)):]
+    
+    model = VAR(df_all)
+    #model.select_order(int(len(df_all)/20))
+    results = model.fit(maxlags=int(len(df_train)/7))
+    forecast = results.forecast(df_train.values, len(df_all)-len(df_train))
+    df_forecast = pd.DataFrame(forecast, columns=df_train.columns)
+
+    return mse(df_forecast['weight'].array, df_test['weight'].array)
 
 
 if __name__ == "__main__":
